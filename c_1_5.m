8d2c9b7f0e40e4ef5c149bce624539ab3081ef52
WALL-E <WALL-E@10.17.91.156>
2021-02-01 16:39:23 +0800

    1.5第三题

---
+++ c_1_5.m
@@ -0,0 +1,17 @@
+n=0:1:30;
+x1=n>=0;
+x2=2.*x1;
+a=1;
+yn1=-1;
+y1=diffeqn(a,x1,yn1);
+y2=diffeqn(a,x2,yn1);
+y3=2.*y1-y2
+subplot(1,2,1);
+stem(n,y1,'r--p');
+xlabel('x1[n]=u[n]');
+subplot(1,2,2);
+stem(n,y2,'m--s');
+xlabel('x2[n]=2*u[n]');
+figure(2);
+stem(n,y3,'^');
+xlabel('2*y1[n]-y2[n]');
