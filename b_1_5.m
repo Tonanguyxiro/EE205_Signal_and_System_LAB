8f95baf7fe2865e80b4a88bef26d25bd55f40fbe
WALL-E <WALL-E@10.17.91.156>
2021-02-01 16:39:06 +0800

    1.5第二题

---
+++ b_1_5.m
@@ -0,0 +1,13 @@
+n=0:1:30;
+x1=n==0;
+x2=n>=0;
+a=1;
+yn1=0;
+y1=diffeqn(a,x1,yn1);
+y2=diffeqn(a,x2,yn1);
+subplot(1,2,1);
+stem(n,y1,'r--p');
+xlabel('x1[n]=δ[n]');
+subplot(1,2,2);
+stem(n,y2,'m--s');
+xlabel('x2[n]=u[n]');
