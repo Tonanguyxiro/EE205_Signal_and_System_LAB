d1a9c9ca5e2b66c567f25e802e632a44f4013c86
WALL-E <WALL-E@10.17.91.156>
2021-02-01 16:39:38 +0800

    1.5第四题

---
+++ d_1_5.m
@@ -0,0 +1,13 @@
+n=0:1:30;
+x=n>=0;
+a=0.5;
+yn1=0;
+y1=diffeqn(a,x,yn1);
+yn1=0.5
+y2=diffeqn(a,x,yn1);
+subplot(1,2,1);
+stem(n,y1,'r--p');
+xlabel('y[-1]=0');
+subplot(1,2,2);
+stem(n,y2,'m--s');
+xlabel('y[-1]=0.5');
