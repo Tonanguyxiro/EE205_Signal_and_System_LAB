b57a6a4b71d0b371046586129b0285f129ae467f
代育津 <11910114@mail.sustech.edu.cn>
2021-03-08 20:28:53 +0800

    update

--- LAB1/P1_5_d.m
+++ LAB1/P1_5_d.m
@@ -7,14 +7,10 @@ y1=diffeqn(a,x,yn1);
 yn1=0.5;
 y2=diffeqn(a,x,yn1);
 
-subplot(2,1,1);
 stem(n,y1,'r--p');
-title('1.5(d)-1');
-xlabel('y[-1]=0');
-
-subplot(2,1,2);
-stem(n,y2,'m--s');
-title('1.5(d)-2');
-xlabel('y[-1]=0.5');
+hold on;
+stem(n,y2,'b--s');
+legend('y[-1]=0','y[-1]=0.5');
+title('1.5(d)');
 
 saveas(gcf, "LAB1/P1_5_d_out.png")
