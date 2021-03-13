71fa20f9d0996a59bce418b829b5707474021348
tongyuan <11810818@mail.sustech.edu.cn>
2021-03-13 12:32:38 +0800

    update LAB 2

---
+++ LAB2/P2_5_a_1.m
@@ -0,0 +1,18 @@
+% Problem 2.5(a) - 1
+
+x1 = [1 0 0 0 0 0];
+x2 = [0 1 0 0 0 0];
+x3 = [1 2 0 0 0 0];
+
+function output = myFun(input)
+%myFun - Description
+%
+% Syntax: output = myFun(input)
+%
+% Long description
+    output = [0 0 0 0 0 0]
+    for i in input
+        if i = 1
+    
+end
+
