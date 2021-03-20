a6908c11f514fe21379010031a3a39b63d2e1949
代育津 <11910114@mail.sustech.edu.cn>
2021-03-20 16:16:02 +0800

    图的title有点问题

--- LAB2/P2_4_d.m
+++ LAB2/P2_4_d.m
@@ -12,8 +12,8 @@ y2 = conv(x1, conv(h1, h2));
 
 ny = nh1(1)+nx1(1):1:nh1(end)+nh1(end)+nx1(end)
 
-subplot(2,1,1), stem(ny, y1), title("y_2[n]=(x_1[n]*h_1[n])*h_2[n]");
-subplot(2,1,2), stem(ny, y2), title("y_1[n]=x_1[n]*(h_1[n]*h_2[n])");
+subplot(2,1,1), stem(ny, y1), title("yd1[n]=(x_1[n]*h_1[n])*h_2[n]");
+subplot(2,1,2), stem(ny, y2), title("yd2[n]=x_1[n]*(h_1[n]*h_2[n])");
 
 saveas(gcf, "plots/P2_4_d_out.png");
 close;
