3702a8f3f19f52132d2dffe0e4148f61048bc3e2
代育津 <11910114@mail.sustech.edu.cn>
2021-03-21 23:14:50 +0800

    update
    加了2.5（f）的图

--- LAB2/P2_5_d.m
+++ LAB2/P2_5_d.m
@@ -71,7 +71,21 @@ z2 = conv(h2, u);
 z2 = z2(1:20);
 
 nz = 0:19;
+figure;
+subplot(2, 1, 1);
+stem(nz,z1);
+title('z1[n]');
+xlabel('n');
+ylabel('z1[n]');
 
+subplot(2, 1, 2);
+stem(nz, z2);
+title('z2[n]');
+xlabel('n');
+ylabel('z2[n]');
+
+saveas(gcf, "plots/P2_5_f_out.png");
+close;
 % 2.5(g) -------------------------------
 
 figure;
